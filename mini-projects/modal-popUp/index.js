df7583487c9ea9acf65870ac6fd53423594c768f
I322214 <sajal.gupta@sap.com>
2022-07-27 00:46:47 +0530

    HTML Modal

---
+++ mini-projects/modal-popUp/index.js
@@ -0,0 +1,18 @@
+const clickBtn = document.querySelector("#modelBtn");
+const closeBtn = document.querySelector("#close");
+const modal = document.querySelector("#simpleModal");
+
+clickBtn.addEventListener("click", function (e) {
+  modal.style.display = "block";
+});
+
+closeBtn.addEventListener("click", function (e) {
+  modal.style.display = "none";
+});
+
+// close the modal when clicked outside anywhere
+window.addEventListener("click", function (e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
