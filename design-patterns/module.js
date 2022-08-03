2501a17aab3fac14b5ec941dc788384db40438d9
I322214 <sajal.gupta@sap.com>
2022-08-03 16:34:09 +0530

    New changes

--- design-patterns/module.js
+++ design-patterns/module.js
@@ -1,24 +1,48 @@
 // 1. Design pattern is used when we want to create private methods and private state.
 // 2. used as a IIFE and exported to be used afterwards
 
-const test = (function () {
-  var contents = "Welcome to India";
-
-  var callInner = function () {
-    console.log(contents);
-    console.log("Welcome Bangalore");
+const Store = (function () {
+  let val = 0;
+  const get = () => {
+    return val;
+  };
+  const set = (data) => {
+    val += data;
   };
 
+  const reset = () => {
+    val = 0;
+  };
   return {
-    // mainMethod: function () {
-    //   callInner();
-    //   console.log(contents);
-    // },
-    mainMethod: callInner,
+    getStore: get,
+    setStore: set,
+    resetStore: reset,
   };
+  // or
+  // return {
+  //    get,
+  //    set,
+  //    reset,
+  // };
 })();
 
-module.exports = test;
+const st1 = Store;
+st1.setStore(10);
+console.log(st1.getStore());
+st1.setStore(10);
+console.log(st1.getStore());
+st1.resetStore();
+console.log(st1.getStore());
 
-test.mainMethod();
+const st2 = Store;
+st2.setStore(10);
+console.log(st2.getStore());
+st2.setStore(10);
+console.log(st2.getStore());
+st2.resetStore();
+console.log(st2.getStore());
+
+// module.exports = Store;
+
+// test.mainMethod();
 // console.log(test.contents);
