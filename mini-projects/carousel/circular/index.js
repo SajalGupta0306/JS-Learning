cf1a6fafe743e1f55c0b1364a943d36f54880f40
Sajal Gupta <gupta.sajal17@gmail.com>
2022-09-24 16:45:04 +0530

    Update in carousal

--- mini-projects/carousel/circular/index.js
+++ mini-projects/carousel/circular/index.js
@@ -7,6 +7,19 @@ const images = [
 const img = document.getElementById("img");
 const forward = document.getElementById("forward");
 const backward = document.getElementById("backward");
+
+// code for adding circles: start
+const circles_parent = document.querySelector(".parent");
+const makeCircles = () => {
+  const span = document.createElement("span");
+  span.classList.add("inner-circle");
+  circles_parent.append(span);
+};
+for (let i = 0; i < images.length - 1; i++) {
+  makeCircles();
+}
+// code for adding circles: end
+
 let currentImgIdx = 0;
 const lastIdx = images.length - 1;
 
@@ -14,18 +27,36 @@ backward.addEventListener("click", function () {
   if (currentImgIdx <= 0) {
     currentImgIdx = lastIdx;
     img.src = images[currentImgIdx];
+    markCircle(currentImgIdx);
     return;
   }
   currentImgIdx -= 1;
   img.src = images[currentImgIdx];
+  markCircle(currentImgIdx);
 });
 
 forward.addEventListener("click", function () {
   if (currentImgIdx >= lastIdx) {
     currentImgIdx = 0;
     img.src = images[currentImgIdx];
+    markCircle(currentImgIdx);
     return;
   }
   currentImgIdx += 1;
   img.src = images[currentImgIdx];
+  markCircle(currentImgIdx);
 });
+
+// code to dynamically circle a child
+function markCircle(index) {
+  const parent = document.querySelector(".parent");
+  const children = Array.from(parent.children);
+  children.forEach((child, idx) => {
+    if (idx === index) {
+      child.classList.remove("checked");
+      child.classList.add("checked");
+    } else {
+      child.classList.remove("checked");
+    }
+  });
+}
