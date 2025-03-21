f40a1a7c9412a64e4e306ff79fb614b653e8be8a
Sajal Gupta <gupta.sajal17@gmail.com>
2025-03-21 10:29:49 +0530

    Update bubble.js

--- algos - sorting_searching/sorting/bubble.js
+++ algos - sorting_searching/sorting/bubble.js
@@ -1,3 +1,20 @@
+// Repeatedly compares adjacent elements and swaps them if they are in the wrong order. 
+// This process is repeated until no swaps are needed, meaning the array is sorted. 
+
+// Time Complexity: 
+//   Best Case: O(n) (when the array is already sorted). 
+//   Average Case: O(n 2 ). 
+//   Worst Case: O (n 2 ) (when the array is sorted in reverse order). 
+
+// Space Complexity: 
+//   O(1) (in-place sorting, no additional memory is used). 
+
+// Use Case: 
+
+// Simple to implement but inefficient for large datasets. 
+// Suitable for small datasets or educational purposes.
+
+
 const arr = [4, 3, 2, 5, 6, 1, 7, 2, 9, 44, 12, 65];
 
 const bubbleSort = (arr) => {
