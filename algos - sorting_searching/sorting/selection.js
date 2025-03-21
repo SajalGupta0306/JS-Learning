f22a2e171fe9692adeefe776ef2bccd0af29413f
Sajal Gupta <gupta.sajal17@gmail.com>
2025-03-21 11:09:30 +0530

    Update selection.js

--- algos - sorting_searching/sorting/selection.js
+++ algos - sorting_searching/sorting/selection.js
@@ -1,3 +1,16 @@
+// Divides the array into a sorted and an unsorted part.
+// Repeatedly selects the smallest (or largest) element from the unsorted part and swaps it with the first unsorted element. 
+
+// Time Complexity: 
+//   Best Case: O(n 2 ). 
+//   Average Case: O(n 2 ). 
+//   Worst Case: O(n 2 ). 
+
+// Space Complexity: 
+//   O(1) (in-place sorting). 
+
+// Use Case: Simple but inefficient for large datasets. Useful when memory usage is a concern.
+
 const arr = [4, 3, 2, 5, 6, 1, 7, 2, 9, 44, 12, 65];
 
 const selectionSort = (arr) => {
