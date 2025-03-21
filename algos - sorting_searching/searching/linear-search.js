278fd8ad8c704a9d5ed1eb0e28d1947f395d3548
Sajal Gupta <gupta.sajal17@gmail.com>
2025-03-21 13:53:03 +0530

    Update linear-search.js

--- algos - sorting_searching/searching/linear-search.js
+++ algos - sorting_searching/searching/linear-search.js
@@ -1,3 +1,15 @@
+// Iterates through each element in the array or list sequentially until the target element is found.
+
+// Time Complexity:
+//   Best Case: O(1) (when the target is the first element).
+//   Average Case: O(n).
+//   Worst Case: O(n) (when the target is the last element or not present).
+
+// Space Complexity: 
+//   O(1) (no additional memory is used).
+
+// Use Case: Suitable for small or unsorted datasets. Simple to implement.
+
 const arr = [1, 2, 3, 4, 5, 8];
 
 const linearSearch = (num, arr) => {
